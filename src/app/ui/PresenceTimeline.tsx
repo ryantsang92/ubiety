a9b3da43752b36ad59d3959c071c21e44adb8e67
Ryan <ryantsang92@gmail.com>
2025-02-13 09:52:38 -0600

    feat: initial start

--- src/app/ui/PresenceTimeline.tsx
+++ src/app/ui/PresenceTimeline.tsx
@@ -1,17 +1,7 @@
 'use client'
 
 import React from "react";
-
-import Highcharts from "highcharts";
-import HighchartsReact from "highcharts-react-official";
-import highchartsDumbbell from "highcharts/modules/dumbbell";
-import HC_more from "highcharts/highcharts-more";
 import { Presence } from "@/app/lib/types";
-import { renderToStaticMarkup } from "react-dom/server";
-import Image from "next/image";
-
-HC_more(Highcharts);
-highchartsDumbbell(Highcharts);
 
 interface PresenceTimelineProps {
   presences: Presence[]
@@ -29,99 +19,117 @@ const PresenceTimeline: React.FC<PresenceTimelineProps> = (props) => {
     })
   }).flat()
 
-  console.log(flattenedPresences)
+  const flattenedPresencesGroupedByProfile = flattenedPresences.reduce((acc, presence) => {
+    if (!acc[presence.name.uid]) {
+      acc[presence.name.uid] = {
+        name: presence.name,
+        presences: []
+      }
+    }
 
-  const categories = flattenedPresences
-    .map((presence) => presence.name)
-    .filter((value, index, self) => self.indexOf(value) === index);
+    acc[presence.name.uid].presences.push(presence)
+    return acc
+  }, {} as Record<number, { name: any, presences: any[] }>)
+
+  console.log(flattenedPresencesGroupedByProfile)
   
-  const chartOptions = {
-    chart: {
-      type: "dumbbell",
-      height: '100%',
-    },
-    legend: {
-      enabled: false
-    },
-    title: {
-      text: "Prescence Timelines"
-    },
-    xAxis: {
-      categories: categories,
-      type: "category",
-      title: {
-        text: "Profiles"
-      },
-      opposite: true,
-      labels: {
-        formatter: function() {
-          const profile = this.value
-          console.log(profile.photo_url)
-          return renderToStaticMarkup(
-            <div>
-              {profile.photo_url && (
-                <img src={profile.photo_url} alt={profile.name} style={{ width: '30px', height: '30px' }} />
-                // <Image
-                //   src={profile.photo_url}
-                //   alt={profile.name}
-                //   className="w-8 h-8 rounded-full"
-                //   width={32}
-                //   height={32}
-                // />
-              )}
-              <span>{profile.name}</span>
-            </div>
-          );
-        }
-      }
-    },
-    yAxis: {
-      title: {
-        text: "Date"
-      },
-      type: 'datetime',
-      dateTimeLabelFormats : {
-        hour: '%I %p',
-        minute: '%I:%M %p'
-      }
-    },
-    series: flattenedPresences.map((presence) => {
-      return {
-        data: [{
-          name: presence.name,
-          low: presence.low,
-          high: presence.high,
-          status: presence.status
-        }],
-        connectorWidth: 3,
-        marker: {
-          enabled: true,
-          symbol: "circle",
-          fillColor: "#4ED304"
-        },
-        connectorColor: "#00E4D9",
-        lowColor: "#00E4D9",
-        dataLabels: {
-          enabled: true,
-          formatter: function() {
-            return Highcharts.dateFormat('%I:%M %p', this.y);
-          }
-        },
-        tooltip: {
-          enabled: true,
-          followPointer: true,
-          headerFormat: `<b>{point.key.name}</b><br>`,
-          pointFormatter: function(): string {
-            return `Status: ${this.status}<br>Time: ${Highcharts.dateFormat('%I:%M %p', this.low)} - ${Highcharts.dateFormat('%I:%M %p', this.high)}`
-          }
-        },
-      }
-    }),
-  }
+  const lines = Object.values(flattenedPresencesGroupedByProfile).map((profile, index) => {
+    return {
+      id: index,
+      image: profile.name.photo_url,
+      dots: profile.presences.map((presence) => {
+        return (presence.low + presence.high) / 2 / 100
+      })
+    }
+  })
+
+  console.log(lines)
+
+  // Generate grid values (0 to 100 by steps of 10)
+  const gridValues = Array.from({ length: 11 }, (_, i) => i * 10);
 
   return (
-    <div>
-      <HighchartsReact highcharts={Highcharts} options={chartOptions} />
+    <div className="w-full h-screen flex items-center justify-center p-8">
+      <div className="relative flex flex-col">
+        <div className="relative flex h-96">
+          {/* Y-axis */}
+          <div className="flex flex-col justify-between mr-4 text-sm text-gray-500">
+            {gridValues.reverse().map((value) => (
+              <div key={value} className="h-4 flex items-center">
+                {value}
+              </div>
+            ))}
+          </div>
+
+          {/* Grid and lines container */}
+          <div className="relative">
+            {/* Horizontal grid lines */}
+            {gridValues.map((value) => (
+              <div
+                key={value}
+                className="absolute w-full border-t border-gray-200"
+                style={{
+                  top: `${100 - value}%`,
+                  left: 0,
+                  right: 0
+                }}
+              />
+            ))}
+
+            {/* Vertical lines with dots */}
+            <div className="flex gap-20 h-full relative pl-12 pr-12">
+              {lines.map((line) => (
+                <div key={line.id} className="relative">
+                  {/* Draw line segments between dots */}
+                  {line.dots.map((_, index) => {
+                    if (index === line.dots.length - 1) return null;
+                    const startPos = line.dots[index];
+                    const endPos = line.dots[index + 1];
+                    const height = (endPos - startPos) * 100;
+                    return (
+                      <div
+                        key={index}
+                        className="absolute w-0.5 bg-blue-500"
+                        style={{
+                          top: `${startPos * 100}%`,
+                          height: `${height}%`
+                        }}
+                      />
+                    );
+                  })}
+                  
+                  {/* Dots */}
+                  {line.dots.map((position, index) => (
+                    <div
+                      key={index}
+                      className="absolute w-2.5 h-2.5 bg-blue-500 rounded-full -left-1"
+                      style={{
+                        top: `${position * 100}%`,
+                        transform: 'translateY(-50%)'
+                      }}
+                    />
+                  ))}
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+
+        {/* X-axis with images */}
+        <div className="flex gap-12 mt-8 ml-20">
+          {lines.map((line) => (
+            <div key={line.id} className="flex flex-col items-center">
+              <div className="w-7 h-7 rounded-full overflow-hidden border-2 border-blue-500">
+                <img 
+                  src={line.image} 
+                  alt="tick mark" 
+                  className="w-full h-full object-cover"
+                />
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   );
 };
