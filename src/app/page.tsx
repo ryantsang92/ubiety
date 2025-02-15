e46df1220e155753f63e7a2ad091c4cee678bdd2
Ryan <ryantsang92@gmail.com>
2025-02-15 16:50:31 -0600

    feat: page test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,14 @@ import PresenceTimeline from "@/app/ui/PresenceTimeline";
 import presence from "@/data/presence.json";
 import profiles from "@/data/profiles.json";
 
-export default function Home() {
+const Home = () => {
   return (
     <div className="p-4 h-full bg-background">
       <div className="text-2xl font-bold text-white">Presence Timeline</div>
-      <div className="bg-white rounded-lg pt-4 pb-4 mt-4 h-full overflow-x-auto">
+      <div
+        className="bg-white rounded-lg pt-4 pb-4 mt-4 h-full overflow-x-auto"
+        data-testid="presence-timeline"
+      >
         <PresenceTimeline
           presenceData={presence}
           profileData={profiles}
@@ -15,3 +18,5 @@ export default function Home() {
     </div>
   );
 }
+
+export default Home;
